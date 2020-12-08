05ca2486e91c30621acc00ea9cdb9aba8b3ed606
hjj14796353396 <1808136161@qq.com>
2020-12-08 22:02:27 +0800

    项目搭建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+<style>
+.icon-bookmark {
+  font-size: 30px;
+  color: red;
+}
+</style>
